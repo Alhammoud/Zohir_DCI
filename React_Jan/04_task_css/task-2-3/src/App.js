88fb148eb04d541c24ac3a4673450d9844d5f702
alhammoud <>
2019-09-18 16:04:58 +0200

    add 04_task_CSS

---
+++ React_Jan/04_task_css/task-2-3/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import './App.css';
+
+
+
+class App extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+
+        Hallo Welt von task-2-3
+      </div>
+    );
+  }
+}
+
+export default App;
+
