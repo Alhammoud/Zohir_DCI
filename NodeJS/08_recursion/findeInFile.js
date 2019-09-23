953bea9d788a8800fbc8b9504f415b8286e8835f
alhammoud <>
2019-09-23 16:06:04 +0200

    add 13_user_table

--- NodeJS/08_recursion/findeInFile.js
+++ NodeJS/08_recursion/findeInFile.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 
 
 // Aufgabe:
@@ -8,6 +9,30 @@
 //    und schaut sich jede Datei genau an. Dabei wird untersucht
 //    ob in der Datei strSearch zu finden ist. Falls ja, 
 //    wird der Pfad der Datei ausgegeben.
+
+// Löusung von Jan ..
+const findInFiles = (path, strSearch) => {
+    let files = fs.readdirSync(path);
+    for (file of files) {
+        // file kann sein datei, verzeichnis oder link
+        // wir schauen uns die datein an .
+        const info = fs.lstatSync(`${path}/${file}`);
+        if (info.isFile()) {
+            // rekursionsende 1
+            // hier gucken wir nach ,ob in der datei ein string zu finden  ist nach muster strSearch
+            // wir öffnen die datei und schauen nach. 
+            const fileContent = fs.readFileSync(`${path}/${file}`);
+            if (new String(fileContent).includes(strSearch)) {
+
+                console.log(` Match : ${path}/${file} `);
+            }
+
+        } else if (info.isDirectory()) {
+            findInFiles(`${path}/${file}`, strSearch);
+        }
+    }
+}
+
 
 // 2) Erstelle eine Funktion (basierend auf findInFiles) namens
 //    replaceInFiles mit drei Parametern path, strSearch und strReplace.
@@ -23,16 +48,15 @@
 
 
 // 1);
-
-const fs = require('fs');
+// Löusung Zohir
 const findInFiles = (path, strSearch) => {
 
-   /*  const info = fs.lstatSync(path);
-    if(!info.isDirectory) return null; */
+    /*  const info = fs.lstatSync(path);
+     if(!info.isDirectory) return null; */
 
     let files = fs.readdirSync(path);
-    for(file of files) {
-        if(fs.lstatSync(`${path}/${file}`).isFile()) {
+    for (file of files) {
+        if (fs.lstatSync(`${path}/${file}`).isFile()) {
 
             /* fs.readFile(`${path}/${file}`, function (err, data) {
                 if (err) throw err;
@@ -41,19 +65,18 @@ const findInFiles = (path, strSearch) => {
                 }
               }); */
             fs.readFile(`${path}/${file}`, function (err, data) {
-                let stringObj=JSON.parse(fs.lstatSync(`${path}/${file}`));
+                let stringObj = JSON.parse(fs.lstatSync(`${path}/${file}`));
                 console.log(stringObj);
 
                 if (err) throw err;
-                if(stringObj.includes(strSearch)){
-                 console.log(data);
+                if (stringObj.includes(strSearch)) {
+                    console.log(data);
                 }
-              });
-        }
-        else if(fs.lstatSync(`${path}/${file}`).isDirectory()) {
+            });
+        } else if (fs.lstatSync(`${path}/${file}`).isDirectory()) {
             findInFiles(`${path}/${file}`, strSearch);
         }
     }
 }
 
-findInFiles("/home/temp/Desktop/Zohir_DCI/NodeJS/07_folderaccess/test",'Rechnung');
+findInFiles("/home/temp/Desktop/Zohir_DCI/NodeJS/07_folderaccess/test", 'Rechnung')
---
+++ NodeJS/13_user_table/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+
+
+// alle dateien ,die im public drin sind ,können von aißen über den dateinamen werden
+// / -> lädt automatisch index.html
+// /main.js -> lädt die datei main.js
+// /style.css -> lädt die datei style.css
+app.use('/',express.static('public'));
+
+
+
+const users = [
+    { id: 1 ,name: 'Stefan', email:'stefan@abc.net',},
+    { id: 2 ,name: 'Lara', email:'lara@gmx.net',},
+    { id: 3 ,name: 'Julia', email:'julia@gmail.com',},
+    { id: 4 ,name: 'Karsten', email:'karsten@foo.net',},
+    { id: 5 ,name: 'Peter', email:'peter@xing.net',}
+]
+
+
+app.get('/users',(req,res) =>{
+    return res.send(users);
+});
+
+/* app.get('/',(req,res) => {
+
+    return res.send('Hallo World ...!');
+})
+ */
+
+console.log('Hallo world from Backend...! (server.js)');
+
+app.listen(3000);
