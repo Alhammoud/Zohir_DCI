1d6ebafa6ddba9e45f36e3b8461ed4c074db7a36
alhammoud <>
2019-09-24 15:55:37 +0200

    ..gemischt

--- NodeJS/13_user_table/public/main.js
+++ NodeJS/13_user_table/public/main.js
@@ -1,1 +1,88 @@
 console.log('Hallo world from Frontend...! (main.js)');
+
+// asynchroner
+/* (async () =>{
+
+})(); */
+
+/* const containerObj = document.getElementById('container');
+ */
+
+
+
+// Aufgabe :
+// Gib dem User Feddback, dass die Daten noch geladen werden.
+// Zeige, solange die Daten geladen werden, einen Lade-Spinner.
+// SChaue mal auf https://loading.io
+
+const loadUsers = async () => {
+    showSpinner();
+    //const result = await fetch ('http://localhost:3000/users');
+    const result = await fetch('http://localhost:3000/users_delay');
+
+    const data = await result.json();
+
+    // const responseStr = JSON.stringify(data);
+
+    console.log('data from server http://localhost:3000/users');
+    console.log(data);
+
+    // Aufgabe :
+    //
+    // Basierend auf diesen Daten.
+    // baue die Tabelle auf.
+
+    let table = `
+                    <table>
+                         <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>e-MAil</th>
+                            <th>Description</th>
+                            <th>Bild</th>
+                         </tr>    
+    `;
+
+    for (user of data) {
+        table += `
+                    <tr>
+                        <th>${user.id}</th>
+                        <th>${user.name}</th>
+                        <th>${user.email}</th>
+                        <th>${user.description}</th>
+                        <th><img src="${user.profilePic}" class="imgUser" alt="" width="200px" height="200px"></th>
+
+                    </tr>    
+        `;
+    }
+
+    table += `</table>`;
+
+    const tableObj = document.getElementById('content');
+
+    tableObj.innerHTML = table;
+}
+
+showSpinner = () => {
+    const showSpinnerObj = document.getElementById('content');
+    showSpinnerObj.innerHTML='<img src="willkommen.gif" alt="willkommen.gif">';
+
+}
+
+console.log('loadUsers(); in main.js');
+console.log(loadUsers());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- NodeJS/13_user_table/server.js
+++ NodeJS/13_user_table/server.js
@@ -1,28 +1,145 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
 
-
-// alle dateien ,die im public drin sind ,können von aißen über den dateinamen werden
+// alle dateien ,die im public drin sind ,können von außen über den dateinamen werden
 // / -> lädt automatisch index.html
 // /main.js -> lädt die datei main.js
 // /style.css -> lädt die datei style.css
-app.use('/',express.static('public'));
+app.use('/', express.static('public'));
 
 
 
-const users = [
+/* const users = [
     { id: 1 ,name: 'Stefan', email:'stefan@abc.net'},
     { id: 2 ,name: 'Lara', email:'lara@gmx.net'},
     { id: 3 ,name: 'Julia', email:'julia@gmail.com'},
     { id: 4 ,name: 'Karsten', email:'karsten@foo.net'},
     { id: 5 ,name: 'Peter', email:'peter@xing.net'}
-]
+]; */
 
 
-app.get('/users',(req,res) =>{
-    return res.send(users);
+app.get('/users', (req, res) => {
+
+    fs.readFile('./users.json', 'utf-8',
+        // wenn das laden der datei fertig ist,
+        // wird die folgende callback-funktion aus-geführt
+
+        (error, data) => {
+            if (error) return res.send({
+                error: error
+            });
+
+            // nur die zusammengefassten Daten herausgeben
+            const usersAll = JSON.parse(data);
+            const usersSummary = [];
+
+
+            for (user of usersAll) {
+                usersSummary.push({
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                });
+            }
+
+            // return res.send(data);
+            return res.send(usersSummary);
+        });
+
+    //return res.send(users);
+
 });
+
+
+// demonstration: langes laden synchron 
+// effekt: blockiert den server, nutzer 2 
+// muss auf nutzer 1 warten
+app.get('/longload_synchron', (req, res) => {
+    let stop = new Date().getTime();
+    while (new Date().getTime() < stop + 10000) {
+        ;
+    }
+
+    return res.send('daten geladen');
+});
+
+
+
+// demonstration: langes laden asynchron 
+// effekt: blockiert den server NICHT, nutzer 2 
+// muss NICHT auf nutzer 1 warten
+app.get('/longload_asynchron', (req, res) => {
+    setTimeout(
+        () => {
+            res.send('daten geladen')
+        },
+        10000
+    )
+});
+
+
+app.get('/users_delay', (req, res) => {
+    setTimeout(
+        () => {
+            fs.readFile('./users.json', 'utf-8',
+                (error, data) => {
+                    if (error) return res.send({
+                        error: error
+                    });
+
+                    return res.send(data);
+                });
+        },
+        10000
+    ); // end setTimeout() 
+});
+
+
+
+
+
+// Aufgabe 2 :
+//
+// 1) In deinem Backend, erstelle eine Ressource :
+//      /userdetail/:id, die einen Parameter :id erwartet.
+//
+// 2) /userdetail/ soll den Nutzer aus users.json laden, mit der Id = id 
+//    und Alle Information zu diesem Nutzer als JSON zurückgeben.
+//    Falls die Id nicht gefunden wurde, dann gib folgendes JSON zurück 
+//    { error : 'user with that id not found ' }
+
+
+// 1)
+app.get('/userdetail/:id', (req, res) => {
+
+    fs.readFile('./users.json', 'utf-8',
+        (error, data) => {
+
+             if (error) return res.send({
+                error: error
+            }); 
+
+           // 2)
+            const usersAll = JSON.parse(data);
+
+
+            for (user of usersAll) {
+                if (user.id == req.params.id) {
+                    return res.send(user)
+                };
+            }
+
+            return res.send({error: 'user with that id not found '});
+
+        }
+    );
+
+});
+
+
+
 
 /* app.get('/',(req,res) => {
 
@@ -33,3 +150,12 @@ app.get('/users',(req,res) =>{
 console.log('Hallo world from Backend...! (server.js)');
 
 app.listen(3000);
+
+
+// Aufgabe  1 :
+//
+// Erweitere die users.json und erstelle für jeden User zwei weitere Felder "description" und "profilePic".
+//
+// 1) Für "description" : schreibe 2 Lustige Sätze über diesen User.
+//
+// 2) Für  "profilePic" : Lade ein Profilbild von irgendeinem Menschen aus dem Internet und speichere es in deinem Public-Ordner. Gib profilePic den Namen der Datei.
