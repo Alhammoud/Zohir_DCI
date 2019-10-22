71daebc9dec5054ca88e540fd6fb83386baea0c2
alhammoud <>
2019-10-22 09:41:24 +0200

    delete datei .git from subfolder

---
+++ NodeJS/MORE_ESOURCES/generate-csv-using-nodejs-mongodb/How to generate CSV using nodejs and mongodb with mongoose_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"406 http:\/\/programmerblog.net\/?p=406","comments":46},{"id":"313 http:\/\/programmerblog.net\/?p=313","comments":51},{"id":"454 http:\/\/programmerblog.net\/?p=454","comments":34},{"id":"753 http:\/\/programmerblog.net\/?p=753","comments":55},{"id":"1328 http:\/\/programmerblog.net\/?p=1328","comments":36}]});
+}
