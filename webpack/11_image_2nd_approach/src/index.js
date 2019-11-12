0a97128d354b78bf88490973ed10a94c5a0008f0
alhammoud <>
2019-11-12 23:58:43 +0100

    added 11_image_2and_approach

---
+++ webpack/11_image_2nd_approach/src/index.js
@@ -0,0 +1,19 @@
+import './index.html';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.scss';
+/* 
+import imageobj from './images/berge_meer.jpg'
+ */
+// dom manipulation
+
+const mainobj = document.getElementById('main');
+
+mainobj.innerHTML= '<h2>Hallo World </h2>';
+
+const successBtn = `<button type="button" class="btn btn-success climateBtn">Klima retten - JETZT</button>`;
+
+mainobj.innerHTML += successBtn;
+
+const image =`<img src="./images/berge_meer.jpg" alt="">`;
+mainobj.innerHTML += image;
