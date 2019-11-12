8b62c33df5c53d9f2fe958e9278325bb3975b1c1
alhammoud <>
2019-11-12 11:07:10 +0100

    added 07_dom

---
+++ webpack/07_dom/src/index.js
@@ -0,0 +1,40 @@
+require('./style.scss');
+
+// const sum = require('./math'); // CommenJS import
+
+const math = require('./math'); // CommenJS import
+
+/* import sum from './math' */ // Es& import 
+
+
+
+import {
+    alphabet,
+    randomstring
+} from './myrandomstring';
+
+console.log(alphabet);
+
+console.log('Random String von 4 Buchstaben : ' + randomstring(4));
+
+
+
+
+// alert('Hallo World - Live Reload');
+
+const sayhallo = () => {
+    console.log(' Hallo World ');
+}
+
+sayhallo();
+
+console.log('sum 1 +3 is ' + math.sum(1, 3));
+
+console.log('Multi 6 +3 is ' + math.multi(6, 3));
+
+
+// dom manipulation
+
+const mainobj = document.getElementById('main');
+
+mainobj.innerHTML= '<h2>Hallo World </h2>'
