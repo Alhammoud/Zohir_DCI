8d98710196f39e33b4ddfb0bf8950511c7d2583a
alhammoud <>
2019-11-16 14:05:24 +0100

    Increment and Decrease number onClick React

---
+++ React_Hadi/dncrement_and_decrease/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+
+    console.log(this.state.show );
+    this.setState({ show: !this.state.show });
+    console.log(this.state.show );
+
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.IncrementItem}>Click to increment by 1</button>
+        <button onClick={this.DecreaseItem}>Click to decrease by 1</button>
+        <button onClick={this.ToggleClick}>
+          { this.state.show ? 'Hide number' : 'Show number' }
+        </button>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
+      </div>
+    );
+  }
+}
+
+export default App;
