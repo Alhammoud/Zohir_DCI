8ba6695b79e6157df28330951520c5c0a735d231
alhammoud <>
2019-11-17 22:36:08 +0100

    add count function with reduce()

--- NodeJS/Javascript_Übung_Hadi/countLetters.js
+++ NodeJS/Javascript_Übung_Hadi/countLetters.js
@@ -6,25 +6,26 @@ module.exports = (arr) => {
     let new_arr = arr[0].split('');
     console.log(" new_arr : ", new_arr);
 
+    count_with_reduce(arr[0]);
 
-    let arr_temp = [];
+    /* let arr_temp = [];
     for (let j = 0; j < new_arr.length; j++) {
 
         if (arr_temp.includes(new_arr[j]) === false) {
             arr_temp.push(new_arr[j]);
-        } /* else {
+        } */ /* else {
             console.log(` ${new_arr[j]} ist schon da ..!`);
         } */
         /*  
        if (arr_temp.includes(new_arr[j])) {
     } 
     */
-    }
+  /*   }
     console.log(arr_temp);
     for (let index = 0; index < arr_temp.length; index++) {
         
         console.log(countLetters(arr_temp[index], new_arr));
-    }
+    } */
     /*
     
     const alphapet ="abcdefghijklmnopqrstuvwxyzöäü"
@@ -67,7 +68,7 @@ function countLetters(letter, new_arr) {
 
 
 
-function check_insert_unique(item) {
+/* function check_insert_unique(item) {
     let unique_list = [];
     let duplicate_list = [];
 
@@ -82,4 +83,37 @@ function check_insert_unique(item) {
     }
 
     return unique_list;
+} */
+
+
+function count_with_reduce(words){
+
+    // const words = ["react", "angular", "react", "react", "redux"];
+  console.log('words');
+  console.log(words);
+    const letters =words.split('');
+    console.log(letters);
+
+
+    const initialObject = {};
+    
+    const letter = letters.reduce(
+      (acc, word) => {
+        if (!acc[word]) {
+          acc[word] = 1;
+        } else {
+          acc[word]++;
+        }
+        return acc;
+      },
+      initialObject
+    );
+    
+    console.log(letter);
+    
+    //output
 }
+
+/* const arr_hello = "helllo hellel ll eller".split('');
+
+console.log(countLetters('e',"helllo hellel ll eller".split(''))); */
